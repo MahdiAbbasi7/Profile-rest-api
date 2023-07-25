d663ff03e3fef0d8b218b37ed78aa8e27cc277a4
Mohammad Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-07-25 14:03:03 +0330

    create admin and superuser for project

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,8 +13,8 @@ class UserProfileManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
-        user.SetPassword(password)
-        user.save(using=self.__db)
+        user.set_password(password)
+        user.save(using=self._db)
 
         return user
 
@@ -24,7 +24,7 @@ class UserProfileManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self.__db)
+        user.save(using=self._db)
 
         return user
 
